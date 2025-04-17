f090264584183e77be0a995376024f790421d9b7
Randira nirman <randiranirman20011210@gmail.com>
2025-04-18 00:00:20 +0530

    login function done

--- src/main/java/org/example/onlinemediclestore/Classes/Order.java
+++ src/main/java/org/example/onlinemediclestore/Classes/Order.java
@@ -2,4 +2,11 @@ package org.example.onlinemediclestore.Classes;
 
 public class Order {
 
+
+    private String medicineName;
+    private int qunatity;
+    private Medicine medicine;
+    private Customer customer;
+
+
 }
--- src/main/java/org/example/onlinemediclestore/Servlets/UserLoginServlet.java
+++ src/main/java/org/example/onlinemediclestore/Servlets/UserLoginServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.example.onlinemediclestore.Classes.PasswordHasher;
 import org.example.onlinemediclestore.Classes.User;
 import org.example.onlinemediclestore.FileHandlers.UserFileHandler;
 
@@ -15,25 +16,27 @@ import java.util.List;
 @WebServlet("/login")
 public class UserLoginServlet  extends HttpServlet
 {
-    protected  void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
         List<User> users = new UserFileHandler().getAllUsers();
 
-
-        for(User user : users){
-            if( user.getUsername().equals(username) && user.getPassword().equals(password)){
-                HttpSession session  =request.getSession();
+        for (User user : users) {
+            if (user.getUsername().equals(username) && PasswordHasher.checkPassword(password, user.getPassword())) {
+                HttpSession session = request.getSession();
                 session.setAttribute("user", user);
-                response.sendRedirect(request.getContextPath() + "dashboard.jsp");
+
+                // Redirect to dashboard on success
+                response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/dashboard.jsp"));
                 return;
             }
         }
 
-
-
-
+        // If login fails
+        request.setAttribute("errorMessage", "Invalid username or password");
+        request.getRequestDispatcher("login.jsp").forward(request, response);
     }
     protected  void doGet (HttpServletRequest request, HttpServletResponse response){
 
