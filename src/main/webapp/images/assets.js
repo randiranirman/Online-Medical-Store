ebec2152dd3bf42124820c0221d721e17600a7db
Randira nirman <randiranirman20011210@gmail.com>
2025-04-19 00:12:46 +0530

    cretead assets.js

---
+++ src/main/webapp/images/assets.js
@@ -0,0 +1,16 @@
+
+
+import  multivitamin from "./multivitamin.jpg"
+import naturesway from "./NATURES.jpg"
+
+import vitaminD from  "./NOW-VITAMIN-D-31000IU-600x600.jpg"
+import vitaminC from "./VITAMIN-C-300x300.jpeg"
+import fishoil from "./WAGNER-FISH-OIL-1000-300x300.jpg"
+
+export const assets = {
+    multivitamin,
+    naturesway,
+    vitaminD,
+    vitaminC,
+    fishoil
+}
